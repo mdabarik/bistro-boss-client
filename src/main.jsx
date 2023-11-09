ccc630e7a4c5b5a2e0014d8d40f18ca0e8bb3c98
mdabarik <mdabarik19@gmail.com>
2023-11-10 01:18:48 +0600

    style added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import router from './routers/router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className='max-w-[1280px] mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
