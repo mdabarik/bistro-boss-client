85bbf9b21f05d9474f437c6c4befa125123e9725
mdabarik <mdabarik19@gmail.com>
2023-11-10 14:28:46 +0600

    slider added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
+import NavBar from '../components/NavBar/NavBar';
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
