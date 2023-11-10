a1dfffd201d0a875e039de8abd7a55051ca385b0
mdabarik <mdabarik19@gmail.com>
2023-11-10 23:25:48 +0600

    dynamic sectionTitle added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import Banner from '../components/Banner/Banner';
+import Category from '../components/Category/Category';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
