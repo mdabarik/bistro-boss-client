84946bcb91828a6a73aab7d71a0b33d21950c2f8
mdabarik <mdabarik19@gmail.com>
2023-11-11 00:18:05 +0600

    featured component added

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,8 +1,22 @@
+import SectionTitle from "../SectionTitle/SectionTitle";
+import FeaturedImg from '../../assets/home/featured.jpg';
+import "./Featured.css";
 
 const Featured = () => {
     return (
-        <div>
-            
+        <div className="featured-item text-white">
+            <SectionTitle subHeading="Check it out" heading="Featured Item" ></SectionTitle>
+            <div className="md:flex items-center justify-center py-8 px-16 md:gap-x-6">
+                <div>
+                    <img src={FeaturedImg} alt="img" />
+                </div>
+                <div>
+                    <p>Aug 20, 2029</p>
+                    <p className="uppercase">Where I can get some?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis odit quibusdam nisi id aliquam dolorem corrupti, ipsa officiis ex, assumenda veritatis non pariatur reiciendis ad amet dolore quam eos temporibus? Ad molestias nam tempora blanditiis numquam ab eum est magni explicabo quis natus quibusdam voluptatem corrupti quaerat, velit officia quas.</p>
+                    <button className="btn btn-outline">Order Now</button>
+                </div>
+            </div>
         </div>
     );
 };
