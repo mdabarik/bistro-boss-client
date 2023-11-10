993643c720b984175ac97b8337b7427befdcb975
mdabarik <mdabarik19@gmail.com>
2023-11-11 00:03:50 +0600

    some component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from '../components/Banner/Banner';
 import Category from '../components/Category/Category';
+import Featured from '../components/Featured/Featured';
+import PopularMenu from '../components/PopularMenu/PopularMenu';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
+            <PopularMenu></PopularMenu>
+            <Featured></Featured>
         </div>
     );
 };
