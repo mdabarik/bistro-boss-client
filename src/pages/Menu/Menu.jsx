42da0390a0293f48de9f28c27a6c645ef26b244f
mdabarik <mdabarik19@gmail.com>
2023-11-11 16:38:03 +0600

    paralx blur added

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../components/Covered/Cover";
+import menuImg from "../../assets/menu/banner3.jpg";
+import PopularMenu from "../../components/PopularMenu/PopularMenu";
 
 const Menu = () => {
     return (
@@ -6,6 +9,8 @@ const Menu = () => {
             <Helmet>
                 <title>Menu - Bisto Boss</title>
             </Helmet>
+            <Cover title="Our Menu" img={menuImg}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
